6b31d410a47007a64043fc4cacbeee43af184711
Ilya22865 <hribanov555@gmail.com>
2026-05-11 20:17:41 +0300

    UPFinal

--- LogisticsWebAPI/obj/Debug/net8.0/LogisticsWebAPI.AssemblyInfo.cs
+++ LogisticsWebAPI/obj/Debug/net8.0/LogisticsWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogisticsWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3ad9a3fdb2913149f3565ca757542b552faf5dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be2ab450e242ee6b36358ef7080b712c8dbd2386")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogisticsWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogisticsWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
